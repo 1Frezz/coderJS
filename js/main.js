3cb97643a198ae1ee424f972210ca2b601c96d43
1Frezz <fac.demarco37@gmail.com>
2024-07-24 23:31:21 -0300

    htmlStructure

--- js/main.js
+++ js/main.js
@@ -1,54 +1,54 @@
-let user = 0
-let pass = 0
+// let user = 0
+// let pass = 0
 
-let database = ["Facundo", 
-            "Malena",
-            "Valentin",
-            1234, 
-            54321, 
-            180322]
+// let database = ["Facundo", 
+//             "Malena",
+//             "Valentin",
+//             1234, 
+//             54321, 
+//             180322]
 
-alert("Bienvenido a tu Homebanking")
+// alert("Bienvenido a tu Homebanking")
 
-login()
+// login()
 
 // Dentro del proyecto agregaría la opción de registro
 
-function login() {
-    user = prompt("Usuario:")
-    pass = prompt("Contraseña:")
+// function login() {
+//     user = prompt("Usuario:")
+//     pass = prompt("Contraseña:")
 
-    let validPass = false
-    let validUser = false
+//     let validPass = false
+//     let validUser = false
 
-    for (let i = 0; i < database.length; i++) {
-        if (database[i] == user) {
-            validUser = true;
-        }
-        if (database[i] == pass) {
-            validPass = true;
-        }
-    }
+//     for (let i = 0; i < database.length; i++) {
+//         if (database[i] == user) {
+//             validUser = true;
+//         }
+//         if (database[i] == pass) {
+//             validPass = true;
+//         }
+//     }
 
-    if(validUser == true && validPass == true){
+//     if(validUser == true && validPass == true){
 
-        alert("Inicio de sesión correcto")
-        console.log("Bienvenido a tu cuenta " + user)
-        console.log("Tu saldo es: $100")
-        options()
-    }else{
-        alert("Datos incorrectos")
-        login()
-    }
-}
+//         alert("Inicio de sesión correcto")
+//         console.log("Bienvenido a tu cuenta " + user)
+//         console.log("Tu saldo es: $100")
+//         options()
+//     }else{
+//         alert("Datos incorrectos")
+//         login()
+//     }
+// }
 
 // Dentro del proyecto haría una función para cada opción con su funcionalidad
 
-function options(){
-    if(confirm("Ingresar a Préstamos") == true){
-        alert("Aún no tenes un préstamo disponible")
-    }else if(confirm("Acceder a mis tarjetas") == true){
-        alert("Sección mis tarjetas")
-        console.log("En esta sección aparecerían todas las tarjetas del cliente")
-    }
-}
+// function options(){
+//     if(confirm("Ingresar a Préstamos") == true){
+//         alert("Aún no tenes un préstamo disponible")
+//     }else if(confirm("Acceder a mis tarjetas") == true){
+//         alert("Sección mis tarjetas")
+//         console.log("En esta sección aparecerían todas las tarjetas del cliente")
+//     }
+// }
